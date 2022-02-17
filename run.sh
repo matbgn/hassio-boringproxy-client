1f4ab612d0c124954a1be73dd69f50b79213d3d2
matbgn <matrash@bqn.ch>
2022-02-18 00:11:47 +0100

    Add basic server url config

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/with-contenv bashio
 
+SERVER_URL=$(bashio::config 'server_url')
+
 wget https://files.apitman.com/public/boringproxy/raw-tls/boringproxy-linux-arm
 mv boringproxy* boringproxy
 chmod +x boringproxy
-./boringproxy client
+./boringproxy client -server SERVER_URL
