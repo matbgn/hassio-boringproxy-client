39cfdcb5c12c2f8e1e48ae33fede31a818e6fb96
matbgn <matrash@bqn.ch>
2022-02-18 00:19:56 +0100

    Add schema to basic configuration interface

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/with-contenv bashio
 
 SERVER_URL=$(bashio::config 'server_url')
+TOKEN=$(bashio::config 'token')
+CLIENT_NAME=$(bashio::config 'client_name')
 
 wget https://files.apitman.com/public/boringproxy/raw-tls/boringproxy-linux-arm
 mv boringproxy* boringproxy
 chmod +x boringproxy
-./boringproxy client -server SERVER_URL
+echo SERVER_URL
+echo TOKEN
+./boringproxy client -server SERVER_URL -token TOKEN -client-name CLIENT_NAME
