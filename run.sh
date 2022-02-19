787832ce7fd14c67b2812c24d72db3b4170901ba
matbgn <matrash@bqn.ch>
2022-02-19 22:52:32 +0100

    Try to fix USER implementation

--- run.sh
+++ run.sh
@@ -2,10 +2,11 @@
 
 SERVER_URL=$(bashio::config 'server_url')
 TOKEN=$(bashio::config 'token')
+USER=$(bashio::config 'user')
 CLIENT_NAME=$(bashio::config 'client_name')
 ACME_EMAIL=$(bashio::config 'acme_email')
 
 wget https://github.com/boringproxy/boringproxy/releases/download/v0.9.1/boringproxy-linux-arm
 mv boringproxy* boringproxy
 chmod +x boringproxy
-./boringproxy client -server ${SERVER_URL} -token ${TOKEN} -client-name ${CLIENT_NAME} -acme-email ${ACME_EMAIL}
+./boringproxy client -server ${SERVER_URL} -token ${TOKEN} -user ${USER} -client-name ${CLIENT_NAME} -acme-email ${ACME_EMAIL}
